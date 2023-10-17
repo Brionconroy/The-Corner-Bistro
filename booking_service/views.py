09f7ca727d5d4100ba687ef6b02f0530e5b44791
Brionconroy <razvanbrion@gmail.com>
2023-10-17 10:01:57 +0000

    Add index.html and base.html and wired them up.

--- booking_service/views.py
+++ booking_service/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views import generic
-from .models import Reservation
 
 
 
+def index(request):
+    return render(request, 'index.html')
+
+def base(request):
+    return render(request, 'base.html')
