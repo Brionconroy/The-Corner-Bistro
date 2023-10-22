ec92726603a3514ae73a522e655c246f355077da
Brionconroy <razvanbrion@gmail.com>
2023-10-22 16:59:00 +0000

    Add base.html file and removed most of the code from index.html

--- booking_service/urls.py
+++ booking_service/urls.py
@@ -5,5 +5,4 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='index'),
     path('base/', views.base, name='base'),
-    path('accounts/', include('allauth.urls')),
 ]
