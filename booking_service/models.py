7422547de19960b259540d006571caa50d1367a7
Brionconroy <razvanbrion@gmail.com>
2023-10-25 17:17:55 +0000

    Update my models.py file

--- booking_service/models.py
+++ booking_service/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils import timezone
 
 
 class Reservation(models.Model):
-    name = models.CharField(max_length=50, blank=False, null=False)
-    naming_id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=50, blank=False, null=False, default='')
+    last_name = models.CharField(max_length=50, blank=False, null=False, default='')
+    naming_id = models.AutoField(primary_key=True)
+    email = models.EmailField(default='')
     date_and_time = models.DateTimeField()
-    num_guests = models.PositiveSmallIntegerField()
+    number_of_guests = models.PositiveSmallIntegerField()
+    special_request = models.TextField(default='')
+    special_requirments = models.TextField(default='')
 
     class Meta:
         ordering = ["naming_id"]
-        
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
